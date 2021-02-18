62f3a1eefbc18b161d756cc031747777eb890695
derMacon <silash169@gmail.com>
2021-02-19 00:28:27 +0100

    somehow possible to set cookie :)

--- token-provider/src/main/java/com/dermacon/jwtauth/controller/AuthenticationController.java
+++ token-provider/src/main/java/com/dermacon/jwtauth/controller/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.dermacon.jwtauth.controller;
 
 import com.dermacon.jwtauth.request.AuthenticationRequest;
+import com.dermacon.jwtauth.response.JWTTokenResponse;
 import com.dermacon.jwtauth.service.AuthenticationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping
@@ -22,9 +25,32 @@ public class AuthenticationController {
         this.authenticationService = authenticationService;
     }
 
+    /**
+     * https://attacomsian.com/blog/set-cookie-with-response-entity-in-spring-boot
+     * @param request
+     * @return
+     */
     @PostMapping("/login")
-    public ResponseEntity createCustomer(@RequestBody AuthenticationRequest request) {
-        return new ResponseEntity<>(authenticationService.generateJWTToken(request.getUsername(), request.getPassword()), HttpStatus.OK);
+    public ResponseEntity createCustomer(@RequestBody AuthenticationRequest request, HttpServletResponse response) {
+        JWTTokenResponse jwtToken = authenticationService.generateJWTToken(request.getUsername(),
+                request.getPassword());
+
+        // create a cookie
+        Cookie cookie = new Cookie("token",jwtToken.getToken());
+
+        // expires in 7 days
+        cookie.setMaxAge(7 * 24 * 60 * 60);
+
+        // optional properties
+        cookie.setSecure(true);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+
+        // add cookie to response
+        response.addCookie(cookie);
+
+        // TODO: add your login logic here
+        return new ResponseEntity<>(jwtToken, HttpStatus.OK);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
