cf2d3464e58803912d335f1261947aa5778a02b9
derMacon <silash169@gmail.com>
2021-02-20 00:47:12 +0100

    added whitelisting

--- model-view/src/main/java/com/dermacon/jwtauth/security/WebSecurityConfig.java
+++ model-view/src/main/java/com/dermacon/jwtauth/security/WebSecurityConfig.java
@@ -35,7 +35,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/new-login");
+        web.ignoring().antMatchers("/new-login", "/all-cookies", "/refresh-token");
     }
 
     @Override
