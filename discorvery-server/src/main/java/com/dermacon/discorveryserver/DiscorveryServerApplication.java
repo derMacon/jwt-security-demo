4351b9f601e286c85cffdabcc5a1b5ee2c0c02be
derMacon <silash169@gmail.com>
2021-02-20 01:26:56 +0100

    fixed typo

--- discorvery-server/src/main/java/com/dermacon/discorveryserver/DiscorveryServerApplication.java
+++ discorvery-server/src/main/java/com/dermacon/discorveryserver/DiscorveryServerApplication.java
@@ -1,4 +1,4 @@
-package com.dremacon.discorveryserver;
+package com.dermacon.discorveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- discorvery-server/src/test/java/com/dermacon/discorveryserver/DiscorveryServerApplicationTests.java
+++ discorvery-server/src/test/java/com/dermacon/discorveryserver/DiscorveryServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dremacon.discorveryserver;
+package com.dermacon.discorveryserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
