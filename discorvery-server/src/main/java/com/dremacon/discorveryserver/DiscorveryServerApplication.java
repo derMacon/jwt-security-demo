b1642ea9e475332b554a78964c5771876a88af4b
derMacon <silash169@gmail.com>
2021-02-20 01:03:10 +0100

    added template project

---
+++ discorvery-server/src/main/java/com/dremacon/discorveryserver/DiscorveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.dremacon.discorveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscorveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscorveryServerApplication.class, args);
+	}
+
+}
