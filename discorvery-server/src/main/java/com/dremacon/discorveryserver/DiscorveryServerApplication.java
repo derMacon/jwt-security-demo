fcdcc95979d0e80b685b965fbbbabd06ac000a14
derMacon <silash169@gmail.com>
2021-02-20 01:20:01 +0100

    discovery server actually running

--- discorvery-server/src/main/java/com/dremacon/discorveryserver/DiscorveryServerApplication.java
+++ discorvery-server/src/main/java/com/dremacon/discorveryserver/DiscorveryServerApplication.java
@@ -2,8 +2,10 @@ package com.dremacon.discorveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscorveryServerApplication {
 
 	public static void main(String[] args) {
