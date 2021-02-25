b102b459a574d441826ab59e183a5905d3635869
derMacon <silash169@gmail.com>
2021-02-25 23:14:22 +0100

    somehow not working as intended

--- token-provider/src/main/java/com/dermacon/jwtauth/data/AppUser.java
+++ token-provider/src/main/java/com/dermacon/jwtauth/data/AppUser.java
@@ -1,5 +1,9 @@
 package com.dermacon.jwtauth.data;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -10,133 +14,22 @@ import javax.persistence.Id;
 
 // todo check if lombok works here
 @Entity
+@Data
+@Builder
 public class AppUser {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long userId;
+    private final long userId;
 
     @Column(unique = true)
-    private String username;
+    private final String username;
 
-    private String password;
+    private final String password;
 
     // todo mail format constraint
-    private String email;
+    private final String email;
 
     @Enumerated(EnumType.STRING)
-    private UserRole role;
-
-    public static class Builder {
-        private String username;
-        private String password;
-        private UserRole role;
-        private String email;
-
-        public Builder username(String username) {
-            this.username = username;
-            return this;
-        }
-
-        public Builder password(String password) {
-            this.password = password;
-            return this;
-        }
-
-        public Builder role(UserRole role) {
-            this.role = role;
-            return this;
-        }
-
-        public Builder email(String email) {
-            this.email = email;
-            return this;
-        }
-
-        public AppUser build() {
-            return new AppUser(this);
-        }
-
-    }
-
-    private AppUser(Builder b) {
-        this.username = b.username;
-        this.password = b.password;
-        this.role = b.role;
-        this.email = b.email;
-    }
-
-    public AppUser() {}
-
-    public long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(long userId) {
-        this.userId = userId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public UserRole getRole() {
-        return role;
-    }
-
-    public void setRole(UserRole role) {
-        this.role = role;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        AppUser appUser = (AppUser) o;
-
-        if (username != null ? !username.equals(appUser.username) : appUser.username != null)
-            return false;
-        if (password != null ? !password.equals(appUser.password) : appUser.password != null)
-            return false;
-        if (email != null ? !email.equals(appUser.email) : appUser.email != null) return false;
-        return role == appUser.role;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = username != null ? username.hashCode() : 0;
-        result = 31 * result + (role != null ? role.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "userId=" + userId +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", role=" + role +
-                '}';
-    }
+    private final UserRole role;
 }
--- token-provider/src/test/java/com/dermacon/jwtauth/controller/RestExceptionHandlerTest.java
+++ token-provider/src/test/java/com/dermacon/jwtauth/controller/RestExceptionHandlerTest.java
@@ -1,31 +1,93 @@
 package com.dermacon.jwtauth.controller;
 
 import com.dermacon.jwtauth.data.AppUser;
+import com.dermacon.jwtauth.data.UserRole;
+import com.dermacon.jwtauth.repository.AccountRepository;
 import com.dermacon.jwtauth.service.AuthenticationService;
+import com.dermacon.jwtauth.service.JwtTokenService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @WebMvcTest(controllers = AuthenticationController.class)
 class RestExceptionHandlerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    /**
+     * Used to serialize test data
+     */
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//
+//    @Autowired
+//    private MockMvc mockMvc;
 
-    @MockBean
-    private AuthenticationService authenticationService;
+//    @Autowired
+//    private AuthenticationController authenticationController;
+
+//    @Autowired
+//    private AuthenticationService authenticationService;
+//
+//    @MockBean
+//    private AccountRepository accountRepository;
+//
+//    @MockBean
+//    private JwtTokenService jwtTokenService;
+//
+//    @MockBean
+//    private PasswordEncoder passwordEncoder;
+
+
+    /**
+     * testing controller advice:
+     * https://stackoverflow.com/questions/15302243/spring-mvc-controllers-unit-test-not-calling-controlleradvice
+     * @throws Exception
+     */
+//    @Before
+//    public void beforeTest() {
+//        MockMvc mockMvc = standaloneSetup(authenticationController)
+//                .setControllerAdvice(new RestExceptionHandler())
+//                .build();
+//    }
 
     @Test
-    public void test1() throws Exception {
-        AppUser test_user = new AppUser();
-        mockMvc.perform(post("/create-token")
-                .contentType("application/json"))
-                .andExpect(status().isOk());
+    public void test_validUser_returns200() throws Exception {
+//        AppUser user = AppUser.builder()
+//                .email("test@mail.com")
+//                .username("admin1")
+//                .password("password")
+//                .role(UserRole.ROLE_ADMIN)
+//                .build();
+//
+//        when(accountRepository.findOneByUsername("admin1")).thenReturn(Optional.empty());
+//
+//        mockMvc.perform(post("/create-token")
+//                .contentType("application/json").content(objectMapper.writeValueAsString(user)))
+//                .andExpect(status().isOk());
+    }
+
+
+
+
+
+    @Test
+    public void test_invalidUser_returns404() throws Exception {
+//        mockMvc = standaloneSetup()
+//                .setControllerAdvice(new YourControllerAdvice())
+//                .build();
     }
 }
