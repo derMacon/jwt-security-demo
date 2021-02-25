fa061711ced029298fde5d2cff314f430c630234
derMacon <silash169@gmail.com>
2021-02-25 22:41:30 +0100

    started implementing tests

--- token-provider/src/test/java/com/dermacon/jwtauth/controller/RestExceptionHandlerTest.java
+++ token-provider/src/test/java/com/dermacon/jwtauth/controller/RestExceptionHandlerTest.java
@@ -1,7 +1,31 @@
 package com.dermacon.jwtauth.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.dermacon.jwtauth.data.AppUser;
+import com.dermacon.jwtauth.service.AuthenticationService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = AuthenticationController.class)
 class RestExceptionHandlerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AuthenticationService authenticationService;
+
+    @Test
+    public void test1() throws Exception {
+        AppUser test_user = new AppUser();
+        mockMvc.perform(post("/create-token")
+                .contentType("application/json"))
+                .andExpect(status().isOk());
+    }
 }
