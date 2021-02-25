5ec0f4b8da8a61ac09c1f065b411072149d8e0ea
derMacon <silash169@gmail.com>
2021-02-25 01:27:32 +0100

    added template presented in the spring docs

--- token-provider/src/main/java/com/dermacon/jwtauth/controller/RestExceptionHandler.java
+++ token-provider/src/main/java/com/dermacon/jwtauth/controller/RestExceptionHandler.java
@@ -10,10 +10,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
@@ -34,10 +37,28 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
      * @return
      */ // todo maybe return Exception as payload???
     // https://stackoverflow.com/questions/52183546/not-able-to-return-responseentity-with-exception-details-in-spring
-    @ExceptionHandler(value = {EntityNotFoundException.class, CredentialsException.class})
-    public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex) {
-        ApiError apiError = new ApiError(NOT_FOUND, ex);
-        return new ResponseEntity(apiError, apiError.getStatus());
+//    @ExceptionHandler(value = {EntityNotFoundException.class, CredentialsException.class})
+//    public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex) {
+//        ApiError apiError = new ApiError(NOT_FOUND, ex);
+//        return new ResponseEntity(apiError, apiError.getStatus());
+//    }
+
+
+    class ErrorInfo {
+        public final String url;
+        public final String ex;
+
+        public ErrorInfo(String url, Exception ex) {
+            this.url = url;
+            this.ex = ex.getLocalizedMessage();
+        }
+    }
+
+    @ResponseStatus(NOT_FOUND)
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseBody ErrorInfo
+    handleBadRequest(HttpServletRequest req, Exception ex) {
+        return new ErrorInfo(req.getRequestURL().toString(), ex);
     }
 
 
