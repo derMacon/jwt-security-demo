f37f8e51559c64c4cae9eeed2e3bf4f61437a493
derMacon <silash169@gmail.com>
2021-02-25 22:32:28 +0100

    added info builder

--- token-provider/src/main/java/com/dermacon/jwtauth/controller/RestExceptionHandler.java
+++ token-provider/src/main/java/com/dermacon/jwtauth/controller/RestExceptionHandler.java
@@ -1,18 +1,14 @@
 package com.dermacon.jwtauth.controller;
 
 import com.dermacon.jwtauth.exception.ApiError;
-import com.dermacon.jwtauth.exception.CredentialsException;
+import com.dermacon.jwtauth.response.ErrorInfo;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.persistence.EntityNotFoundException;
@@ -22,7 +18,7 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 /**
  * source: https://www.toptal.com/java/spring-boot-rest-api-error-handling
- *
+ * <p>
  * overview http headers: https://www.baeldung.com/cs/http-status-codes
  */
 @Order(Ordered.HIGHEST_PRECEDENCE)
@@ -33,35 +29,21 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
      * Handles exception thrown by service.
      * To centralize exception handling use @ControllerAdvice annotation
      * see: https://howtodoinjava.com/spring-core/spring-exceptionhandler-annotation/
+     *
      * @param ex
      * @return
-     */ // todo maybe return Exception as payload???
-    // https://stackoverflow.com/questions/52183546/not-able-to-return-responseentity-with-exception-details-in-spring
-//    @ExceptionHandler(value = {EntityNotFoundException.class, CredentialsException.class})
-//    public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex) {
-//        ApiError apiError = new ApiError(NOT_FOUND, ex);
-//        return new ResponseEntity(apiError, apiError.getStatus());
-//    }
-
-
-    class ErrorInfo {
-        public final String url;
-        public final String ex;
-
-        public ErrorInfo(String url, Exception ex) {
-            this.url = url;
-            this.ex = ex.getLocalizedMessage();
-        }
-    }
-
+     */
     @ResponseStatus(NOT_FOUND)
     @ExceptionHandler(EntityNotFoundException.class)
-    @ResponseBody ErrorInfo
+    @ResponseBody
+    ErrorInfo
     handleBadRequest(HttpServletRequest req, Exception ex) {
-        return new ErrorInfo(req.getRequestURL().toString(), ex);
+        return ErrorInfo.builder()
+                .url(req)
+                .status(NOT_FOUND)
+                .exception(ex)
+                .build();
     }
-
-
 
 
 //    @Override
---
+++ token-provider/src/test/java/com/dermacon/jwtauth/controller/RestExceptionHandlerTest.java
@@ -0,0 +1,7 @@
+package com.dermacon.jwtauth.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestExceptionHandlerTest {
+
+}
